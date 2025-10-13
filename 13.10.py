b818cb075adea5f3de0d9dc67c233e9dd185724d
Взрослая академия <dsafannikov9@gmail.com>
2025-10-13 21:16:29 +0300

    commit

---
+++ 13.10.py
@@ -0,0 +1,47 @@
+# list1 = [1,2,3,5,32,5,2,45]
+# print(list1)
+# for i in range (0,9):
+#     print(list1[i])
+# print()
+# for i in list1:
+#     print(i)
+
+
+# list2=[2,5,3,5,3,6,2]
+# counter=0
+# for i in range(len(list2)):
+#    for j in range(len(list2)):
+#         if i==j:
+#             continue
+#         if list2[i]==list2[j]:
+#          counter=+1
+#          break
+# print(len(list2)-counter)
+
+# list1=[2,4,3,7,3,8]
+# for i in range(len(list1)//2):
+#     list1[i],list1[len(list1)-i-1]=list1[len(list1)-i-1],list1[i]
+# print(list1)
+
+# list1=[3,5,5,3,6,9,54,4,3]
+# summa=0
+# for i in range(len(list1)):
+#     summa+=list1[i]
+# print(summa/len(list1))
+
+# list1=[3,5,3,7,3,6,3]
+# list2=[5,3,6,7,12,1]
+# for i in list1:
+#     for j in list2:
+#         if i==j:
+#             print(i)
+#             break
+# while True:
+#     try:
+#         a=int(input("введите число"))
+#         print("число")
+#         break
+#     except:
+#         print("не число")
+
+
